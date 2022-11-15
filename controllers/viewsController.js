863b237a7b778f8fd763501afa07a2b594972b35
manthony <41161951+manthonyg@users.noreply.github.com>
2022-11-14 22:09:45 -0500

    project work updating to loans

---
+++ controllers/viewsController.js
@@ -0,0 +1,31 @@
+
+
+exports.getLandingPAge = async (req, res) => {
+  res.status(200).render('overview', {
+    title: `Over View`
+  
+  });
+};
+
+exports.getCourse = async (req, res) => {
+  res.status(200).render('Course', {
+    title: `Get Course`
+  });
+};
+exports.createNewCourse = async (req, res) => {
+  res.status(200).render('newCourse', {
+    title: `Create New Course`
+  });
+};
+
+exports.getSignInForm = (req, res) => {
+  res.status(200).render('signIn', {
+    title: 'Sign in New User'
+  });
+};
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
